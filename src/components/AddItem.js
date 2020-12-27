6a1903fb3d869a55afa8f935f4a2367763457a12
ebox-huh <differentmax9@gmail.com>
2020-12-27 02:21:34 -0800

    Remove unused import

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -4,7 +4,6 @@ import './Item.css';
 
 import Grid       from '@material-ui/core/Grid';
 import Paper      from '@material-ui/core/Paper';
-import Switch     from '@material-ui/core/Switch';
 import Button     from '@material-ui/core/Button';
 import TextField  from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
