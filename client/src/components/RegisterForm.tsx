bd02c6072f8f4f19ec3a92ac315ada391d923c59
maxmwang <orangemax888@gmail.com>
2022-10-24 00:27:37 -0700

    Fix register form sends login request

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Stack, Button } from '@mui/material';
 
 import { useAppDispatch } from '../redux/hooks';
-import { userLogin } from '../redux/constants/actionCreators/userActions';
+import { userRegister } from '../redux/constants/actionCreators/userActions';
 
 import useInputWithErrors from '../hooks/inputWithErrors';
 
@@ -42,7 +42,7 @@ function RegisterForm({ className, toLogin }: Props) {
     e.preventDefault();
 
     if (validate()) {
-      dispatch(userLogin(inputs.username, inputs.password));
+      dispatch(userRegister(inputs.username, inputs.password));
     }
   };
 
