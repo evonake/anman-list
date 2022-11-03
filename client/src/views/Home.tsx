e2aba3cdcaf8a299224b57d2fb081d94fa19056e
maxmwang <orangemax888@gmail.com>
2022-11-03 02:16:24 -0700

    Show itemlists as tabs on home page

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import {
+  Tab,
+  Tabs,
+} from '@mui/material';
 
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { itemGetThunk, selectItemsLists } from '../redux/features/itemListsSlice';
+
+import type { TypeDBItem } from '../types/item';
 
 import ItemList from '../components/ItemList';
 import AddItem from '../components/AddFab';
@@ -14,6 +21,12 @@ function Home() {
 
   const itemLists = useAppSelector(selectItemsLists);
 
+  const [currentListIndex, setCurrentListIndex] = useState(0);
+
+  const handleTabChange = (n: number) => {
+    setCurrentListIndex(n);
+  };
+
   useEffect(() => {
     dispatch(itemGetThunk());
   }, []);
@@ -23,12 +36,40 @@ function Home() {
     <div id="home">
       <h1>Home</h1>
       <LogoutButton />
-      {itemLists.map((list) => (
-        <div key={list._id}>
+      <Tabs
+        value={currentListIndex}
+        onChange={(e, n) => handleTabChange(n)}
+      >
+        {itemLists.map((list, i) => (
+          <Tab label={list.name} value={i} />
+        ))}
+      </Tabs>
+
+      {itemLists.map((list, i) => (
+        <TabPanel key={list._id} id={list._id} show={i === currentListIndex}>
           <ItemList itemList={list} />
           <AddItem listId={list._id} />
-        </div>
+        </TabPanel>
       ))}
+    </div>
+  );
+}
+
+type TabPanelProps = React.PropsWithChildren & {
+  show: boolean;
+  id: TypeDBItem['_id'];
+};
+function TabPanel({ show, id, ...props }: TabPanelProps) {
+  const { children } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={show}
+      id={`simple-tabpanel-${id}`}
+      aria-labelledby={`simple-tab-${id}`}
+    >
+      {children}
     </div>
   );
 }
